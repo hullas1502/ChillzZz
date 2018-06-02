0190baf7354e830019d48b8ecddd907f8c8f9f70
gulsezim11 <gulsezimduisen@gmail.com>
2018-06-02 19:58:04 +0600

    changes

--- resultjs.js
+++ resultjs.js
@@ -1,4 +1,40 @@
 $( document ).ready(function() {
+	var resto_pairs = pairs;
+	var randnum1 = getRandomInt(pairs.length);
+	var randnum2 = getRandomInt(pairs.length);
+	var randnum3 = getRandomInt(pairs.length);
+	var randnum4 = getRandomInt(pairs.length);
+	var randnum5 = getRandomInt(pairs.length);
+	var randnum6 = getRandomInt(pairs.length);
+	
+    var resto1 = pairs[randnum1].resto;
+	var resto2 = pairs[randnum2].resto;
+	var resto3 = pairs[randnum3].resto;
+	var resto4 = pairs[randnum4].resto;
+	var resto5 = pairs[randnum5].resto;
+	var resto6 = pairs[randnum6].resto;
+	
+	var pic1 = pairs[randnum1].pic;
+	var pic2 = pairs[randnum2].pic;
+	var pic3 = pairs[randnum3].pic;
+	var pic4 = pairs[randnum4].pic;
+	var pic5 = pairs[randnum5].pic;
+	var pic6 = pairs[randnum6].pic;
+	
+	$("#resto1").text(resto1);
+	$("#resto2").text(resto2);
+	$("#resto3").text(resto3);
+	$("#resto4").text(resto4);
+	$("#resto5").text(resto5);
+	$("#resto6").text(resto6);
+	
+	document.getElementById("pic1").src= pic1;
+	document.getElementById("pic2").src= pic2;
+	document.getElementById("pic3").src= pic3;
+	document.getElementById("pic4").src= pic4;
+	document.getElementById("pic5").src= pic5;
+	document.getElementById("pic6").src= pic6;
+	
 	if(sessionStorage.getItem('star')!=null){
 		$(".star").attr("src", "star"+sessionStorage.getItem('star')+".PNG");
 		$("#h5").text("Restaurants with spice rate "+sessionStorage.getItem('star'))
@@ -6,4 +42,7 @@ $( document ).ready(function() {
 	if(sessionStorage.getItem("location") != "")
 		$(".address").text("Address: "+sessionStorage.getItem("location"));
 });
+function getRandomInt(max){
+	return Math.floor(Math.random()*(max));	
+}
 
